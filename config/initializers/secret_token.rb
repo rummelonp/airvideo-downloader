10db125ad1844ea269cbb6527475a405ad214018
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-30 19:27:08 +0900

    AirVideo downloader initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirvideoDownloader::Application.config.secret_token = '28ef92b535a433b1b9384f037e4766b18d79b71e442fae502d83bc1ffed32ea09b083d447717e7efb70ba28f877499551677f2e14f18c981139b49e5738de031'

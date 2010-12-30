9047599bd6d88e47e97f566d13b15abb396daed2
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-30 23:22:34 +0900

    Add recent method.

--- app/models/video.rb
+++ app/models/video.rb
@@ -6,4 +6,11 @@ class Video < ActiveRecord::Base
     video
   end
 
+  def self.recent
+    find(:all,
+         conditions: ['downloaded = ? and encoded = ?', true, true],
+         order: 'id desc',
+         limit: 10)
+  end
+
 end

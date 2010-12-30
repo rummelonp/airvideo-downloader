543ac0cc5b52a5083461837ec9b9e0e5ff6c098d
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-30 21:44:24 +0900

    Add index controller spec.

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -2,25 +2,42 @@ require 'spec_helper'
 
 describe IndexController do
 
-  describe "GET 'status'" do
-    it "should be successful" do
-      get 'status'
-      response.should be_success
+  describe 'GET "/"' do
+    context :response do
+      before { get :status }
+      subject { response }
+      it { should be_success }
     end
   end
 
-  describe "GET 'parse'" do
-    it "should be successful" do
-      get 'parse'
-      response.should be_success
-    end
-  end
+  describe 'GET "/parse"' do
 
-  describe "GET 'download'" do
-    it "should be successful" do
-      get 'download'
-      response.should be_success
+    context 'widhout url' do
+      before { get :parse }
+
+      describe :response do
+        subject { response }
+        it { should be_redirect }
+        describe :redirect_to do
+          subject { URI.parse(response.redirect_url).request_uri }
+          it { should == '/' }
+        end
+      end
+
+      describe :flash, :notice do
+        subject { request.session['flash'][:notice] }
+        it { should == 'Please specify the url.' }
+      end
     end
+
+    context 'with url' do
+      describe :response do
+        before { get :parse, url: 'http://example.com/video_url' }
+        subject { response }
+        it { should be_success }
+      end
+    end
+
   end
 
 end

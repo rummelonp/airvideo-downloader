9c1a43c17df04d2fb690fbd9b15726f936d9585c
Kazuya Takeshima <mail@mitukiii.jp>
2010-12-30 22:53:07 +0900

    Add recent spec.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,4 +7,16 @@ describe Video do
     it { should be_instance_of(Video) }
   end
 
+  describe :recent do
+    before { @recent = Video.recent }
+    describe do
+      subject { @recent }
+      it { should have_at_most(10).items }
+    end
+    describe do
+      subject { @recent.first }
+      it { should be_instance_of(Video) }
+    end
+  end
+
 end

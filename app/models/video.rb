7b7a2c5781dbfc5711d857966fdb41653ffe2818
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-02 16:44:20 +0900

    Refactring.

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,44 +1,43 @@
 class Video < ActiveRecord::Base
-
   VIDEO_DIR     = '/var/media/movie'
-
   ENCODE_SUFFIX = ' - airvideo'
   ENCODE_EXT    = '.m4v'
 
-  def self.parse(url)
-    downloader = Downloader.parse url
-    video = self.new
-    video.url = downloader.url
-    video.title = downloader.title
-    video.video_url = downloader.video_url
-    video.download_path =
-      File.join VIDEO_DIR, download_filename(video.title, video.video_url)
-    video.encoded_path =
-      File.join VIDEO_DIR, encoded_filename(video.title, video.video_url)
-    video
-  end
+  class << self
+    def parse(url)
+      downloader = Downloader.parse url
+      video = self.new
+      video.url = downloader.url
+      video.title = downloader.title
+      video.video_url = downloader.video_url
+      video.download_path =
+        File.join VIDEO_DIR, download_filename(video.title, video.video_url)
+      video.encoded_path =
+        File.join VIDEO_DIR, encoded_filename(video.title, video.video_url)
+      video
+    end
 
-  def self.recent
-    find(:all,
-         conditions: ['downloaded = ? and encoded = ?', true, true],
-         order: 'id desc',
-         limit: 10)
-  end
+    def recent
+      find(:all,
+           conditions: ['downloaded = ? and encoded = ?', true, true],
+           order: 'id desc',
+           limit: 10)
+    end
 
-  def self.filename(title)
-    title.split(' ').map(&:downcase).join('_')
-  end
+    def filename(title)
+      title.split(' ').map(&:downcase).join('_')
+    end
 
-  def self.extname(video_url)
-    File.extname(video_url).split(%r{[?&]}).first
-  end
+    def extname(video_url)
+      File.extname(video_url).split(%r{[?&]}).first
+    end
 
-  def self.download_filename(title, video_url)
-    "#{filename(title)}#{extname(video_url)}"
-  end
+    def download_filename(title, video_url)
+      "#{filename(title)}#{extname(video_url)}"
+    end
 
-  def self.encoded_filename(title, video_url)
-    "#{filename(title)}#{ENCODE_SUFFIX}#{ENCODE_EXT}"
+    def encoded_filename(title, video_url)
+      "#{filename(title)}#{ENCODE_SUFFIX}#{ENCODE_EXT}"
+    end
   end
-
 end

d1ad86f759acff4027e9a1e4fa440d26b1ebaead
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-02 17:04:09 +0900

    Refactring video model specs.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Video do
 
-  describe :parse do
+  describe 'parse "http://example.com/video_url"' do
     before { Video.stub(:parse).and_return(Video.new) }
     subject { Video.parse('http://example.com/video_url') }
     it { should be_instance_of(Video) }
@@ -13,14 +13,9 @@ describe Video do
       Video.stub(:recent).and_return([Video.new] * 10)
       @recent = Video.recent
     end
-    describe do
-      subject { @recent }
-      it { should have_at_most(10).items }
-    end
-    describe do
-      subject { @recent.first }
-      it { should be_instance_of(Video) }
-    end
+    subject { @recent }
+    it { should have_at_most(10).items }
+    it { subject.first.should be_instance_of(Video) }
   end
 
   describe 'title "Test Title" and video_url "http://example.com/video_url.flv"' do

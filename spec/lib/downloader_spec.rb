7fd071e0d97ac0459045764d156a12bcdc7e1642
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-02 17:24:58 +0900

    Add download spec.

--- spec/lib/downloader_spec.rb
+++ spec/lib/downloader_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'tempfile'
 
 describe Downloader do
 
@@ -8,6 +9,22 @@ describe Downloader do
     end
     subject { Downloader.parse 'http://example.com/youtube_video_url' }
     it { should be_instance_of(Downloader::YouTube) }
+  end
+
+  describe :download do
+    before do
+      @download_file = Tempfile.new('test_video.flv')
+      @download_path = @download_file.path
+      Downloader.stub(:download).and_return(@download_file)
+    end
+    describe :download_path do
+      subject { Downloader.download('http://example.com/video_url', @download_path) }
+      it { subject.path.should == @download_path }
+    end
+    describe :file do
+      subject { File }
+      it { should be_exists @download_file }
+    end
   end
   
 end

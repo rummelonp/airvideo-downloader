110daff39b05f8c5b4d71d3d64820281dd6f243e
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-02 17:15:35 +0900

    Add parse spec.

--- spec/lib/downloader_spec.rb
+++ spec/lib/downloader_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Downloader do
+
+  describe 'parse "http://example.com/youtube_video_url"' do
+    before do
+      Downloader.stub(:parse).and_return(Downloader::YouTube.new)
+    end
+    subject { Downloader.parse 'http://example.com/youtube_video_url' }
+    it { should be_instance_of(Downloader::YouTube) }
+  end
+  
 end

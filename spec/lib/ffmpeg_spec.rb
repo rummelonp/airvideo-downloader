a3f08600eab9a8379e52ebcdfc4589d3f4956b03
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-03 15:20:48 +0900

    Add encode spec.

--- spec/lib/ffmpeg_spec.rb
+++ spec/lib/ffmpeg_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 require 'tempfile'
 
 describe Ffmpeg do
+
+  describe :encode do
+    before do
+      @encoded_file = Tempfile.new('test_video - airvideo.m4v')
+      Ffmpeg.stub(:encode).and_return(nil)
+    end
+
+    it 'file should be exists' do
+      File.should be_exists @encoded_file
+    end
+  end
+
 end

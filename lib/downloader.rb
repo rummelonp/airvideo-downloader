ee98c6fc4dc679ee32ac730fb113d0743a3ec13c
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-03 15:45:27 +0900

    Add rdoc comments.

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -6,11 +6,19 @@ require 'shellwords'
 
 Dir[Rails.root.join("lib/downloader/*.rb")].each {|f| require f}
 
+# Wrapper for video downloads
 module Downloader
   extend self
 
+  # When you use the delayed jobs is that Delayed::Worker.Logger to
+  # Otherwise the RAILS_DEFAULT_LOGGER
   LOGGER = Delayed::Worker.logger || RAILS_DEFAULT_LOGGER
 
+  # Parsing from video url and returned downloader wrapper
+  # Param:: (String) video url
+  # Return:: Downloader::Base subclass instance
+  # Raise:: Downloader::ParserError:
+  #         raised when not implements parser
   def parse(url)
     host = URI.parse(url).host
     parser = Base.parsers.find {|p| p.host === host} or
@@ -18,6 +26,10 @@ module Downloader
     parser.parse(url)
   end
 
+  # Downloading from video url and download path
+  # Param:: (String) video url,
+  #         (String) download path
+  # Return:: nil
   def download(video_url, download_path)
     LOGGER.info `wget -O #{download_path.shellescape} '#{video_url}' 2>&1`
   end

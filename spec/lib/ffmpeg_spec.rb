01957391022b610e4b2a1d09b180c2db2d41a21a
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-03 15:23:38 +0900

    Refactring ffmepg ecnode spec.

--- spec/lib/ffmpeg_spec.rb
+++ spec/lib/ffmpeg_spec.rb
@@ -5,8 +5,12 @@ describe Ffmpeg do
 
   describe :encode do
     before do
+      @download_file = Tempfile.new('test_video.flv')
       @encoded_file = Tempfile.new('test_video - airvideo.m4v')
+      @download_path = @download_file.path
+      @encoded_path = @encoded_file.path
       Ffmpeg.stub(:encode).and_return(nil)
+      Ffmpeg.encode(@download_path, @encoded_path)
     end
 
     it 'file should be exists' do

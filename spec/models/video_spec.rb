a48d6d3f6f27d02a55d13c650e24d213bc3de5b2
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-03 14:41:50 +0900

    Add proccess spec.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,17 +2,18 @@ require 'spec_helper'
 
 describe Video do
 
+  before do
+    @valid_data = {
+      url: 'http://example.com/video_url',
+      title: 'Test Title',
+      video_url: 'http://example.com/video_url.flv',
+      download_path: '/var/media/movie/test_title.flv',
+      encoded_path: '/var/media/movie/test_title - airvideo.m4v'
+    }
+    @invalid_data = @valid_data.merge url: nil
+  end
+
   describe :validates do
-    before do
-      @valid_data = {
-        url: 'http://example.com/video_url',
-        title: 'Test Title',
-        video_url: 'http://example.com/video_url.flv',
-        download_path: '/var/media/movie/test_title.flv',
-        encoded_path: '/var/media/movie/test_title - airvideo.m4v'
-      }
-      @invalid_data = @valid_data.merge url: nil
-    end
     context 'presence all' do
       subject { Video.new @valid_data }
       it { should be_valid }
@@ -20,6 +21,27 @@ describe Video do
     context 'presence without url' do
       subject { Video.new @invalid_data }
       it { should_not be_valid }
+    end
+  end
+
+  describe :proccess do
+    before do
+      @video = Video.new @valid_data
+      Downloader.stub(:download).and_return(nil)
+      Ffmpeg.stub(:encode).and_return(nil)
+    end
+    describe :download, :downloaded do
+      before { @video.download }
+      subject { @video.downloaded }
+      it { should be_true }
+    end
+    describe :encode, :encoded do
+      before do
+        @video.downloaded = true
+        @video.encode
+      end
+      subject { @video.encoded }
+      it { should be_true }
     end
   end
 

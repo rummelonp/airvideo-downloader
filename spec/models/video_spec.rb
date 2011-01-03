6504bbece81a55b24d50ebfe5a37195baa923a69
Kazuya Takeshima <mail@mitukiii.jp>
2011-01-03 14:02:30 +0900

    Add validates spec.

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,6 +2,27 @@ require 'spec_helper'
 
 describe Video do
 
+  describe :validates do
+    before do
+      @valid_data = {
+        url: 'http://example.com/video_url',
+        title: 'Test Title',
+        video_url: 'http://example.com/video_url.flv',
+        download_path: '/var/media/movie/test_title.flv',
+        encoded_path: '/var/media/movie/test_title - airvideo.m4v'
+      }
+      @invalid_data = @valid_data.merge url: nil
+    end
+    context 'presence all' do
+      subject { Video.new @valid_data }
+      it { should be_valid }
+    end
+    context 'presence without url' do
+      subject { Video.new @invalid_data }
+      it { should_not be_valid }
+    end
+  end
+
   describe 'parse "http://example.com/video_url"' do
     before { Video.stub(:parse).and_return(Video.new) }
     subject { Video.parse('http://example.com/video_url') }
